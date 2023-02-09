b61c2134da3bcb332af37b7545fda8a8ff54350b
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-09 20:18:35 -0300

    feat: character name on dialogs

--- src/scripts/npc_quimic.gd
+++ src/scripts/npc_quimic.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+
+
 var dialogues = [
-	"Olá! Tudo bem? Eu sou o seu gerente. Você deve ser o novo funcionário da Unipar, correto? Seja bem-vindo!",
-	"Olá! Sou eu mesmo. O novo funcionário! Estou aqui para aprender sobre o dia a dia na empresa e quais protocolos devo seguir para manter a minha segurança.",
-	"Certo! Você já pode conhecer as áreas da nossa fábrica e suas regras. Mas antes, pegue os EPI's e fique seguro! Ocorreu um acidente em uma fábrica de uma de nossas concorrentes aqui em Camaçari e não queremos que o mesmo acnteça aqui! Vamos lá! Você tem muito a fazer!",
-	"Pode deixar! Estou super confiante para o primeiro dia!."
+	{'name': 'Químico', 'text': "Olá! Tudo bem? Eu sou o seu gerente. Você deve ser o novo funcionário da Unipar, correto? Seja bem-vindo!"},
+	{'name': 'Operador', 'text': "Olá! Sou eu mesmo. O novo funcionário! Estou aqui para aprender sobre o dia a dia na empresa e quais protocolos devo seguir para manter a minha segurança."},
+	{'name': 'Químico', 'text': "Certo! Você já pode conhecer as áreas da nossa fábrica e suas regras. Mas antes, pegue os EPI's e fique seguro! Ocorreu um acidente em uma fábrica de uma de nossas concorrentes aqui em Camaçari e não queremos que o mesmo aconteça aqui! Vamos lá! Você tem muito a fazer!"},
+	{'name': 'Operador', 'text': "Pode deixar! Estou super confiante para o primeiro dia!."}
 ]
 
 var can_interact = false
@@ -26,14 +28,20 @@ func close_dialogue():
 	$dialog.visible = false
 	current = 0
 
-func _process(_delta):
-	if (not $dialog.visible and can_interact and Input.is_action_pressed("interact")):
-		print('teste')
-		load_dialogue()
-
-func _on_Button_pressed():
+func next_dialogue():
 	if current >= len(dialogues):
 		close_dialogue()
 		return
-	$dialog/RichTextLabel.text = dialogues[current]
+	$dialog/name.text = dialogues[current].get('name')
+	$dialog/RichTextLabel.text = dialogues[current].get('text')
 	current += 1
+
+func _on_Button_pressed():
+	next_dialogue()
+
+func _input(event):
+	if ($dialog.visible and event.is_action_released('skip')):
+		next_dialogue()
+
+	if (not $dialog.visible and can_interact and event.is_action_pressed("interact")):
+		load_dialogue()
