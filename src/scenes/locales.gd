8bfb3270d140f66fe42c3fb8c727badee9c9ffa5
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-11 09:39:11 -0300

    feat: languages support

---
+++ src/scenes/locales.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var language = 'pt-BR'
+
+var locale = {}
+var characters = {}
+var dialogs = {}
+
+func loadLanguage():
+	var file = File.new()
+	file.open('res://locales/' + language + '.json', File.READ)
+	var data = parse_json(file.get_as_text())
+	locale = data
+
+func _ready():
+	loadLanguage()
+	characters = locale.get('characters')
+	dialogs = locale.get('dialogs')
--- src/scripts/npc_quimic.gd
+++ src/scripts/npc_quimic.gd
@@ -1,18 +1,12 @@
 extends Area2D
 
-# diálogos dos personagens
-var dialogues = [
-	{'name': 'Químico', 'text': 'Olá! Tudo bem? Eu sou o seu gerente. Você deve ser o novo funcionário da Unipar, correto? Seja bem-vindo!'},
-	{'name': 'Operador', 'text': 'Olá! Sou eu mesmo. O novo funcionário! Estou aqui para aprender sobre o dia a dia na empresa e quais protocolos devo seguir para manter a minha segurança.'},
-	{'name': 'Químico', 'text': 'Certo! Você já pode conhecer as áreas da nossa fábrica e suas regras. Mas antes, pegue os EPI\'s e fique seguro! Ocorreu um acidente em uma fábrica de uma de nossas concorrentes aqui em Camaçari e não queremos que o mesmo aconteça aqui! Vamos lá! Você tem muito a fazer!'},
-	{'name': 'Operador', 'text': 'Pode deixar! Estou super confiante para o primeiro dia!.'}
-]
-
 # flag para indicar se o jogador está na área de interação com o NPC
 var canInteract = false
 
 # representa o diálogo atual (índice na lista)
 var currentDialog = 0
+
+onready var locales = $"../Locales"
 
 
 func _on_npc_quimico_body_entered(_body):
@@ -41,15 +35,15 @@ func closeDialogue():
 
 func nextDialogue():
 	# passa para a próxima frase do diálogo
-	if currentDialog >= len(dialogues):
+	if currentDialog >= len(locales.dialogs):
 		# se acabaram as frases, finaliza o diálogo
 		closeDialogue()
 		return
 
 	# altera o nome do personagem para o da fala atual
-	$Dialog/Name.text = dialogues[currentDialog].get('name')
+	$Dialog/Name.text = locales.characters.get(locales.dialogs[currentDialog].get('character'))
 	# altera o texto para a fala atual
-	$Dialog/RichTextLabel.text = dialogues[currentDialog].get('text')
+	$Dialog/RichTextLabel.text = locales.dialogs[currentDialog].get('text')
 	# atualiza o índice do diálogo atual
 	currentDialog += 1
 
