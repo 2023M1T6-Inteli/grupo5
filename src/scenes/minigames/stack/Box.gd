f0423db3733f0c87100ff74ea33f5d88715f7e7e
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-15 15:51:57 -0300

    feat: stack mechanic

---
+++ src/scenes/minigames/stack/Box.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+export (int) var gravity = 3000
+export (int) var horizontalVelocity = 0
+var velocity = Vector2()
+
+var falling = false
+
+func _physics_process(delta):
+	if falling:
+		velocity.x = 0
+		velocity.y += gravity * delta
+		velocity = move_and_slide(velocity, Vector2(0, -1))
+	else:
+		if position.x > 960:
+			horizontalVelocity = -50000
+		elif position.x < 325:
+			horizontalVelocity = 50000
+
+		print(position.x)
+		velocity.x = horizontalVelocity * delta
+		velocity = move_and_slide(velocity, Vector2(-11, 0))
+
+func _input(event):
+	if event.is_action_pressed("interact"):
+		falling = true
---
+++ src/scripts/minigames/Stack.gd
@@ -0,0 +1,76 @@
+extends Node2D
+
+var lastBoxPosition = Vector2(630, 610)
+onready var currentBox = $Box
+var playing = true
+var lastRegionWidth = 500
+var rect = Rect2()
+var score = 0
+var currentRectSize = Vector2(500, 50)
+var currentRectPosition = -250
+var currentCollisionShape = Vector2(250, 25)
+var currentCollisionPosition = 0
+
+
+func _ready():
+	print(currentBox.get_node("ColorRect").rect_size)
+	print(currentBox.get_node("ColorRect").rect_position.x)
+	
+	print(currentBox.get_node("CollisionShape2D").shape.extents)
+	print(currentBox.get_node("CollisionShape2D").position.x)
+
+
+func duplicateBox():
+	cropBox()
+	lastBoxPosition = currentBox.position
+	currentBox = preload("res://scenes/minigames/stack/Box.tscn")
+	currentBox = currentBox.instance()
+	currentBox.set_script(load("res://scenes/minigames/stack/Box.gd"))
+	add_child(currentBox)
+	updateBox()
+
+
+func cropBox():
+	var deltaPosition = (lastBoxPosition.x - currentBox.position.x) * -1
+	currentRectSize = Vector2(currentRectSize.x-abs(deltaPosition), 50)
+
+	if deltaPosition < 0:
+		currentRectPosition -= deltaPosition
+
+	currentCollisionShape = Vector2((currentCollisionShape.x-abs(deltaPosition)/2), 25)
+	currentCollisionPosition -= (deltaPosition/2)
+	updateBox()
+
+func updateBox():
+	currentBox.get_node("ColorRect").rect_size = currentRectSize
+	currentBox.get_node("ColorRect").rect_position.x = currentRectPosition
+
+	currentBox.get_node("CollisionShape2D").shape.extents = currentCollisionShape
+	currentBox.get_node("CollisionShape2D").position.x = currentCollisionPosition
+
+func win():
+	playing = false
+	currentBox.get_node("ColorRect").modulate = Color(0, 1, 0, 1)
+	print('ganhou oba')
+	
+
+func lose():
+	playing = false
+	currentBox.get_node("ColorRect").modulate = Color(1, 0, 0, 1)
+	print('perdeu af')
+
+
+func _process(_delta):
+	if playing:
+		if currentBox.position.y >= lastBoxPosition.y:
+			lose()
+			return
+
+		if currentBox.is_on_floor():
+			score += 1
+			if score == 10:
+				win()
+				return
+			duplicateBox()
+
+	$ScoreLabel.text = str(score)
