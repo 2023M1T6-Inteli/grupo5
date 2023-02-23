8a9099a8bae664835c28bde66e03639fb285b0bc
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-23 17:19:45 -0300

    fix: clipboard animation only in y

--- src/scripts/Clipboard.gd
+++ src/scripts/Clipboard.gd
@@ -1,49 +1,72 @@
 extends Button
 
-# flag que indica que a prancheta está recuando
-var closing = false
+var animationPlaying = false
+var isOpened = false
 
-func _on_Animation_animation_finished(_anim_name):
-	if closing:
-		# ao terminar a animação de recuo, mostra o botão de interação novamente
-		get_parent().get_node("Gamepad/interact").show()
+func slideToScreen():
+	animationPlaying = true
+	$Tween.interpolate_property(
+		self,
+		'rect_position:y',
+		570,
+		130,
+		0.3,
+		Tween.TRANS_SINE,
+		Tween.EASE_IN_OUT
+	)
+	$Tween.start()
 
-	# reseta a flag de recuo
-	closing = false
+
+func slideOffScreen():
+	animationPlaying = true
+	$Tween.interpolate_property(
+		self,
+		'rect_position:y',
+		130,
+		570,
+		0.3,
+		Tween.TRANS_SINE,
+		Tween.EASE_IN_OUT
+	)
+	$Tween.start()
 
 
 func _on_mouse_entered():
 	# ao passar o mouse na prancheta, ela aparece na tela
-	if $Animation.is_playing():
+	if animationPlaying or isOpened:
 		# impede que as animações entrem em conflito
 		return
 
-	# esconde o botão de interação
-	get_parent().get_node("Gamepad/interact").hide()
 	# inicia a animação
-	$Animation.play('slideToScreen')
+	slideToScreen()
+	isOpened = true
 
 
 func _on_mouse_exited():
 	# ao tirar o mouse da prancheta, ela recua
-	if $Animation.is_playing():
+	if animationPlaying:
 		# impede que as animações entrem em conflito
 		return
 
 	# inicia a animação ao contrário
-	$Animation.play_backwards('slideToScreen')
-	# atualiza a flag de recuo
-	closing = true
+	slideOffScreen()
+	isOpened = false
 
 
 func _ready():
 	# coleta o nome do nível de acordo com o node root
-	var levelName = $"../..".name
 	# atualiza o nome na prancheta
-	$LevelName.bbcode_text = '[center]' + Locales.levels.get(levelName) + '[/center]'
+	$LevelName.bbcode_text = '[center]' + Locales.levels.get(Global.currentLevel) + '[/center]'
 
 	# atualiza todos os textos da prancheta de acordo com o nível
 	var nodeCounter = 0
-	for node in get_tree().get_nodes_in_group('LevelText'):
-		node.bbcode_text = '[center]' + Locales.clipboard.get(levelName)[nodeCounter] + '[/center]'
+	for node in get_children():
+		if not node.is_in_group('LevelText'):
+			continue
+
+		node.bbcode_text = '[center]' + Locales.clipboard.get(Global.currentLevel)[nodeCounter] + '[/center]'
 		nodeCounter += 1
+
+
+func _on_tween_completed(_object, _key):
+	animationPlaying = false
