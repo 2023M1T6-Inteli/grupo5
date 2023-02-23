e25765afa90ee03ec6ac9bbae10c0d39db4cecc2
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-23 17:20:52 -0300

    fix: limit on wood box

--- src/scripts/minigames/WoodBox.gd
+++ src/scripts/minigames/WoodBox.gd
@@ -4,6 +4,9 @@ var woodCounter = 2
 
 
 func _on_WoodBox_pressed():
+	if woodCounter < 0:
+		return
+
 	var newWood = load('res://scenes/Minigames/Pallets/PalletWood.tscn')
 	newWood = newWood.instance()
 	newWood.position = get_global_mouse_position()
