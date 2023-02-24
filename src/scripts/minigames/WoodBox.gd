33d0d25ef2737237e00dfebc9f3f3648ce582ed1
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-02-24 11:44:41 -0300

    fix: error with interact getting pallet

--- src/scripts/minigames/WoodBox.gd
+++ src/scripts/minigames/WoodBox.gd
@@ -4,6 +4,9 @@ var woodCounter = 2
 
 
 func _on_WoodBox_pressed():
+	if $"..".palletSelected:
+		return
+
 	if woodCounter < 0:
 		return
 
@@ -11,6 +14,8 @@ func _on_WoodBox_pressed():
 	newWood = newWood.instance()
 	newWood.position = get_global_mouse_position()
 	newWood.selected = true
-	get_parent().add_child_below_node(self, newWood)
+	get_parent().add_child_below_node($"../FixedWood4", newWood)
 	texture_normal = load('res://assets/minigames/woodBox/' + str(woodCounter) + '.png')
 	woodCounter -= 1
+
+	$"..".palletSelected = newWood
