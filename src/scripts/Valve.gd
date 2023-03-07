cbfb1fd9388bf7d18bfcc69084f2bfb43ac4d8a4
anna-aragao <anna.aragao@sou.inteli.edu.br>
2023-03-07 12:52:49 -0300

    Feat: moving platform

--- src/scripts/Valve.gd
+++ src/scripts/Valve.gd
@@ -3,13 +3,13 @@ extends Area2D
 export (String) var Valves = 'valve'
 var canInteract = false
 
-func _on_Valve_body_entered(body):
+func _on_Valve_body_entered(_body):
 	$Stroke.visible = true
 	canInteract = true
 	get_parent().get_node("HUD/Gamepad/interact").modulate = Color(1, 1, 0.4)
 
 
-func _on_Valve_body_exited(body):
+func _on_Valve_body_exited(_body):
 	$Stroke.visible = false
 	canInteract = false
 	get_parent().get_node("HUD/Gamepad/interact").modulate = Color(1, 1, 1)
