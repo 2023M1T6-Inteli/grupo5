aebdb795b97f90bd73f44f5d15fe2d35797418af
Vitto-Mazeto <Vitto.Mazeto@sou.inteli.edu.br>
2023-03-09 15:51:39 -0300

    fix: fadein/ fadeout time

--- src/scripts/Music.gd
+++ src/scripts/Music.gd
@@ -1,6 +1,5 @@
 extends AudioStreamPlayer
 
-onready var tween = $Tween
 var minVolume = -80
 var volume = -15
 
@@ -12,37 +11,39 @@ var musics = {
 	'Level1' : load("res://assets/sounds/music/8bit_move.ogg")
 }
 
+
 func fadeIn():
-	tween.interpolate_property(
+	# chama o tween para executar a função de fade in da música
+	$Tween.interpolate_property(
 		self, 
 		'volume_db', 
 		minVolume, 
 		volume, 
-		1.3, 
+		1.0, 
 		Tween.TRANS_LINEAR, 
 		Tween.EASE_IN
 	)
-	tween.start()
+	$Tween.start()
 
 
 func fadeOut():
-	tween.interpolate_property(
+	$Tween.interpolate_property(
 		self, 
 		'volume_db', 
 		volume, 
 		minVolume, 
-		0.5, 
+		0.2, 
 		Tween.TRANS_LINEAR, 
 		Tween.EASE_IN
 	)
-	tween.start()
+	$Tween.start()
 
 
 func _on_Tween_tween_completed(_object, _key):
 	# espera o fadeout acabar para iniciar o fadein da próxima música
 	if !fadingOut:
 		return
-	#acessa o dicionário das musicas
+	# acessa o dicionário das musicas
 	stream  = musics[currentMusic]
 	fadeIn()
 	play()
