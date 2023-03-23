54c3220e54eeee6caa1bae9f99d86c238570e289
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-03-23 11:10:55 -0300

    fix: locales on responsability term

--- src/scripts/Level1/ResponsabilityTerm.gd
+++ src/scripts/Level1/ResponsabilityTerm.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
 
+func _ready():
+	$characterName.text = Global.characterName
+	$Title.text = Locales.others.responsabilityTerm.title
+	$Term.text = Locales.others.responsabilityTerm.term
+	$Signature.text = Locales.others.responsabilityTerm.signature
+
+
 func closeMinigame():
 	# fecha o minigame, desligando a indicação e limpando a instância da cena
 	Global.minigameRunning = false
+	Global.currentLevel = 'Level1'
 	queue_free()
 
 
 # Função confirma a assinatura do usuário no termo de responsabilidade fazendo-o retornar ao jogo.
-func _on_AssignButton_pressed():
+func _on_Signature_pressed():
+	$characterName.visible = true
+	yield(get_tree().create_timer(1), "timeout")
 	closeMinigame()
