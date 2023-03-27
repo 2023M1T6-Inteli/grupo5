2cd7a7b21c04abf10bf38a81b2c6157ef0a8b64c
Eduardo Santos Barreto <eduardo.barreto@sou.inteli.edu.br>
2023-03-27 15:52:46 -0300

    feat: E interaction on elevator

--- src/scripts/Elevator.gd
+++ src/scripts/Elevator.gd
@@ -3,18 +3,64 @@ extends Area2D
 var canInteract = false
 
 
+func showE():
+	# mostra o E na tela, em cima do químico
+	$E.visible = true
+	$Tween.interpolate_property(
+		$E,
+		'position:y',
+		-235,
+		-260,
+		0.2
+	)
+	$Tween.interpolate_property(
+		$E,
+		'modulate:a',
+		0,
+		1,
+		0.3
+	)
+	$Tween.start()
+
+
+func hideE():
+	# esconde o E na tela, de cima do químico
+	$Tween.interpolate_property(
+		$E,
+		'position:y',
+		-260,
+		-235,
+		0.2
+	)
+	$Tween.interpolate_property(
+		$E,
+		'modulate:a',
+		1,
+		0,
+		0.3
+	)
+	$Tween.start()
+
+
 func _on_Elevator_body_entered(_body):
 	# quando o player se aproxima do elevador executa a animação de abertura
 	$Animation.play('open')
 	canInteract = true
 	# muda a cor do botão de interagir
 	get_parent().get_node('HUD/Gamepad/interact').modulate = Color(1, 1, 0.4)
+	showE()
 
 
 func _on_Elevator_body_exited(_body):
 	$Animation.play('close')
 	canInteract = false
 	get_parent().get_node('HUD/Gamepad/interact').modulate = Color(1, 1, 1)
+	hideE()
+
+
+func changeLevel():
+	if get_tree().change_scene('res://scenes/' + Global.currentLevel + '.tscn') != OK:
+		print('An unexpected error occured when trying to switch to next scene')
 
 
 func _input(event):
@@ -25,11 +71,14 @@ func _input(event):
 		return
 
 	# faz a troca de cena baseada na variável global do nível
-	if Global.currentLevel == 'Level1':
+	if Global.currentLevel == 'Tutorial':
+		Global.currentLevel = 'Level1'
+		changeLevel()
+
+	elif Global.currentLevel == 'Level1':
 		Global.currentLevel = 'Level2'
-		if get_tree().change_scene('res://scenes/Level2.tscn') != OK:
-			print('An unexpected error occured when trying to switch to next scene')
+		changeLevel()
+
 	elif Global.currentLevel == 'Level2':
 		Global.currentLevel = 'Level3'
-		if get_tree().change_scene('res://scenes/Level3.tscn') != OK:
-			print('An unexpected error occured when trying to switch to next scene')
+		changeLevel()
