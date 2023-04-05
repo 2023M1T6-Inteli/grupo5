94828bc469c957c217b81093eeb95f235da93f95
Vitto-Mazeto <Vitto.Mazeto@sou.inteli.edu.br>
2023-04-05 10:31:22 -0300

    add: end 3 languages

--- src/scripts/End.gd
+++ src/scripts/End.gd
@@ -1,19 +1,10 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	$Title2.text = Locales.others.end.title
+	$Title2/Subtitle.text = Locales.others.end.subtitle
+	$Credits.text = Locales.others.end.credits
 
 
 func _on_Ranking_pressed():
